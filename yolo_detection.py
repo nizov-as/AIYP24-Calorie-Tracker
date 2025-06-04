559773188ef348c6c6cd36c70bcbbea454342d01
denis <denis.kruglov@wheelies.ru>
2025-06-05 00:48:35 +0300

    add onnx format

--- yolo_detection.py
+++ yolo_detection.py
@@ -1,19 +1,11 @@
 from ultralytics import YOLO
 import cv2
-import torch
 import numpy as np
 from typing import Dict, Tuple
-from torch.nn.modules.container import Sequential
-
-torch.serialization.add_safe_globals([
-    torch.nn.Module,
-    Sequential,
-    __import__('ultralytics.nn.tasks').nn.tasks.DetectionModel
-])
 
 class FoodDetector:
     def __init__(self, model_path):
-        self.model = YOLO(model_path)
+        self.model = YOLO(model_path.replace('.pt', '.onnx'))
         self.confidence_threshold = 0.4
 
     def detect_and_visualize(self, image_bytes: bytes) -> Tuple[Dict[str, float], bytes]:
