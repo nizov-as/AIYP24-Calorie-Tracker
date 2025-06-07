ae10d2cef9aa3b07327c69b274af37d0a9b90c16
denis <denis.kruglov@wheelies.ru>
2025-06-07 15:45:04 +0300

    final bot

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import asyncio
 import logging
 import os
-import threading
-from aiohttp import web
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
+from aiohttp import web
 from aiogram.fsm.storage.redis import RedisStorage
 import redis.asyncio as redis
 from config import TOKEN
@@ -27,13 +26,11 @@ async def handle_healthcheck(request):
 
 # Запуск HTTP-сервера в отдельном потоке
 def run_web_app():
-    port = int(os.getenv('PORT', 8000))  # Используем PORT из окружения или 8000 по умолчанию
     app = web.Application()
     app.add_routes([web.get('/', handle_healthcheck)])
-    logger.info(f"Starting HTTP server on port {port}")
-    web.run_app(app, port=port, host='0.0.0.0')
+    web.run_app(app, port=8000, host='0.0.0.0')
 
-# Настраиваем хранилище состояний на Redis
+# Настраиваем хранилище состояний на Redis (берём из переменных окружения)
 redis_conn = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=int(os.getenv("REDIS_PORT")),
@@ -42,24 +39,21 @@ redis_conn = redis.Redis(
 )
 storage = RedisStorage(redis=redis_conn)
 
-# Создаем экземпляры бота и диспетчера
+# 🤖 Создаем экземпляры бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher(storage=storage)
 
-# Подключаем middleware и handlers
+# 🔗 Подключаем middleware и handlers
 dp.message.middleware(LoggingMiddleware())
 setup_handlers(dp)
 
 async def main():
-    # Запускаем HTTP-сервер в фоне
-    threading.Thread(target=run_web_app, daemon=True).start()
-    
     logger.info("Бот запущен ✅")
     try:
         await dp.start_polling(bot)
     finally:
         await redis_conn.aclose()
-        await storage.aclose()
+        await storage.close()
 
 if __name__ == "__main__":
     try:
